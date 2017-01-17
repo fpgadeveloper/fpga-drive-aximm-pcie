ea629dce0434349c7d02a0fbdc7aaed2659e2f12
Jeff Johnson <jeff@fpgadeveloper.com>
2017-01-17 12:39:24 -0500

    updated for Vivado 2016.4

--- SDK/build-sdk.bat
+++ SDK/build-sdk.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
 setlocal ENABLEDELAYEDEXPANSION
-SET sdk=C:\Xilinx\SDK\2016.3\bin\xsdk.bat
+SET sdk=C:\Xilinx\SDK\2016.4\bin\xsdk.bat
 cmd /c "%sdk% -batch -source build-sdk.tcl"
 pause
--- Vivado/build-ac701.bat
+++ Vivado/build-ac701.bat
@@ -1,2 +1,2 @@
-SET vivado=C:\Xilinx\Vivado\2016.3\bin\vivado.bat
+SET vivado=C:\Xilinx\Vivado\2016.4\bin\vivado.bat
 %vivado% -mode batch -source build-ac701.tcl
